577d6bb09846cd0de5a8e81e24233442a9ea73ab
codedjay672 <emmajoel6456@gmail.com>
2025-08-15 11:35:40 +0100

    setup login page

--- mobile/app/(auth)/login.tsx
+++ mobile/app/(auth)/login.tsx
@@ -1,10 +1,23 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Pressable, Text, View } from "react-native";
 
 const SignIn = () => {
   return (
-    <View className="w-full flex-1 flex justify-center items-center bg-red-400">
-      <Text className="text-3xl">Sign In</Text>
+    <View className="w-full flex-1 flex justify-center items-center bg-black relative">
+      <View className="w-full space-y-1">
+        <Text className="text-2xl text-green-600 font-semibold tracking-widest text-center">
+          spotlight
+        </Text>
+        <Text className="text-sm text-green-700 text-center">
+          sign in to continue
+        </Text>
+      </View>
+
+      <Pressable className="w-2/3 mx-auto bg-black border border-green-500 rounded-full p-2 absolute bottom-10 left-auto">
+        <Text className="text-base text-green-700 font-bold text-center">
+          Continue with google
+        </Text>
+      </Pressable>
     </View>
   );
 };
