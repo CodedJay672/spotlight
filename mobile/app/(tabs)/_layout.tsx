9e7a093a73f5675ab5a7b92b4619930632540b68
codedjay672 <emmajoel6456@gmail.com>
2025-08-16 11:39:25 +0100

    implement a custom tab layout for easy navigation

--- mobile/app/(tabs)/_layout.tsx
+++ mobile/app/(tabs)/_layout.tsx
@@ -1,18 +1,62 @@
 import React from "react";
 import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
 
 const TabsLayout = () => {
   return (
     <Tabs
       screenOptions={{
         headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: "black",
+          width: "100%",
+          height: 20,
+          paddingTop: 6,
+          elevation: 50,
+        },
       }}
     >
-      <Tabs.Screen name="index" />
-      <Tabs.Screen name="create" />
-      <Tabs.Screen name="notifications" />
-      <Tabs.Screen name="profile" />
-      <Tabs.Screen name="saves" />
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <Ionicons name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="notifications"
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <Ionicons name="heart" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="create"
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <Ionicons name="add-circle" size={size} color="green" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="saves"
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <Ionicons name="bookmark" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <Ionicons name="person" size={size} color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 };
--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,10 +1,10 @@
-import { StatusBar } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { ClerkLoaded, ClerkProvider } from "@clerk/clerk-expo";
 import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import "./globals.css";
 import { Stack } from "expo-router";
 import InitialRootLayout from "@/components/InitialRootLayout";
+import { StatusBar } from "react-native";
 
 export default function RootLayout() {
   const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
@@ -16,6 +16,7 @@ export default function RootLayout() {
       <ClerkLoaded>
         <SafeAreaProvider>
           <SafeAreaView style={{ flex: 1, backgroundColor: "black" }}>
+            <StatusBar barStyle="light-content" />
             <InitialRootLayout />
           </SafeAreaView>
         </SafeAreaProvider>
